7d19f1fff17c04d793457eebe6579e5ff7b5ba15
paulina.piekarska <paulina.piekarska@HK-10>
2023-10-29 13:32:29 +0100

    Add project files.

---
+++ Zad/Controllers/DaneController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zad.Models;
+
+namespace Zad.Controllers
+{
+    public class DaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }else return View();
+           
+        }
+        public IActionResult Wynik(Dane dane)
+        {
+
+            return View(dane);
+        }
+
+    }
+}
---
+++ Zad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
